b2ea4eaecbd9399a14fa86c25772d4de9d69a7b9
Frutios <benjamin-tourneur_student2020@wilder.school>
2021-01-25 21:24:55 +0100

    routing show

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,16 +6,35 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * Affiche tous les programmes
-     * @Route("/program", name="program_index")
-     */
+    * Affiche tous les programmes
+    * @Route("/", name="index")
+    */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
         ]);
     }
+
+
+    /**
+    * Affiche un programme en détail
+    * @Route("/{id}", name="show", 
+    * requirements={"id"="\d+"},
+    * methods={"GET"})
+    */
+
+    public function show(int $id)
+    {
+        return $this->render('program/show.html.twig',[
+            'id' => $id
+        ]);
+    }
+
 }
